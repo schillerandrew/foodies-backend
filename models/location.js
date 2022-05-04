31b351f075d62bb76ebc5f926a1de2d33ff061a1
michael <mcbrunette33@gmail.com>
2022-05-04 11:36:20 -0700

    refresing updates

--- models/location.js
+++ models/location.js
@@ -4,10 +4,8 @@ let axios = require('axios');
 
 async function getLocation (req, res, next) {
 try{
-  console.log(req.query)
 let locationName = req.query.q;
 let locationApiUrl =`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_API_KEY}&q=${locationName}&format=json`;
-console.log(locationApiUrl);
 let locationData = await axios.get(locationApiUrl);
 res.send(locationData.data);
 }catch(err) {
