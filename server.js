86dced1c2542b6a213b01dc317a779617c0c413a
schillerandrew <schiller.andrew@gmail.com>
2023-02-13 15:15:36 -0800

    trying different syntax for adding ACAO header

--- server.js
+++ server.js
@@ -15,13 +15,19 @@ app.use(cors());
 app.use(express.json());
 
 // adds Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
 });
 
 const PORT = process.env.PORT || 3001;
