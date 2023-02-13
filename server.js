1ab5110a4bd6ee999effc5bd92d891119ad880b2
schillerandrew <schiller.andrew@gmail.com>
2023-02-13 11:57:56 -0800

    switched CORS back to specific origin header in server.js

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// const corsOptions = {
-//   origin: "https://foodies-schiller.cyclic.app",
-// }
+const corsOptions = {
+  origin: "https://foodies-schiller.netlify.app",
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 const PORT = process.env.PORT || 3001;
 const mongoose = require('mongoose');
